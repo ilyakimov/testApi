d63eb5b22a9462e0696f6a31ba9c7aae1d5db630
ilyakimov <iyakimov@plotpad.ru>
2019-09-11 18:32:47 +0300

    maxDelayed test

--- src/test/java/com/wiley/AbstractRestITTest.java
+++ src/test/java/com/wiley/AbstractRestITTest.java
@@ -1,16 +1,11 @@
 package com.wiley;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.request;
-import static org.hamcrest.core.IsEqual.equalTo;
 
-import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import java.util.Collection;
-import java.util.List;
+
 
 abstract class AbstractRestITTest {
 
@@ -19,19 +14,25 @@ abstract class AbstractRestITTest {
         .setContentType(ContentType.JSON)
         .setAccept(ContentType.JSON)
         .setBaseUri("https://www.wiley.com")
-            .setBasePath("/en-us/search/autocomplete/comp_00001H9J")
+        .setBasePath("/en-us/search/autocomplete/comp_00001H9J")
         .build();
   }
 
-  void get() {
-    given()
-        .contentType(ContentType.JSON)
-        .accept(ContentType.JSON)
-        .get("https://www.wiley.com/en-us/search/autocomplete/comp_00001H9J?term=Java")
+  static RequestSpecification postSpecification() {
+    return new RequestSpecBuilder()
+        .setContentType(ContentType.JSON)
+        .setAccept(ContentType.JSON)
+        .setBaseUri("https://httpbin.org")
+        .setBasePath("/delay/")
+        .build();
+  }
+
+  protected int postDelay() {
+    return given()
+        .spec(postSpecification())
+        .post("2")
         .then()
-        .assertThat()
-        .statusCode(200)
-        .and()
-        .body("suggestions.temp", equalTo("<span class=\"search-highlight\">java</span>script"));
+        .extract()
+        .statusCode();
   }
 }
--- src/test/java/com/wiley/RestIT.java
+++ src/test/java/com/wiley/RestIT.java
@@ -1,22 +1,19 @@
 package com.wiley;
 
 import static io.restassured.RestAssured.given;
+import static org.awaitility.Awaitility.await;
 import static org.hamcrest.Matchers.matchesPattern;
-import static org.hamcrest.core.IsEqual.equalTo;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import io.restassured.RestAssured;
-import io.restassured.common.mapper.TypeRef;
-import io.restassured.http.ContentType;
+
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
 import java.util.Random;
 
-import org.hamcrest.Matcher;
+import java.util.concurrent.TimeUnit;
+import org.awaitility.Awaitility;
 import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,37 +25,41 @@ class RestIT extends AbstractRestITTest {
   private static final String spanRegExStarting = "<span class=\"search-highlight\">java</span>.*";
   private static final String spanRegExIncludes = ".*<span class='search-highlight'>Java</span>.*";
   private static final String nameWiley = ".*Wiley.*";
-  @DisplayName("Rest GET collection")
+
   @Test
-  void getTerm() throws IOException {
-    Random rnd = new Random();
+  void getTest() throws IOException {
     JsonNode node = given()
-            .spec(getSpecification())
-            .queryParam("term", "Java")
+        .spec(getSpecification())
+        .queryParam("term", "Java")
         .get()
         .then()
         .assertThat()
         .statusCode(200)
         .and()
         .assertThat()
-            .body("suggestions[0].term", matchesPattern(spanRegExStarting))
-            .body("suggestions[1].term", matchesPattern(spanRegExStarting))
-            .body("suggestions[2].term", matchesPattern(spanRegExStarting))
-            .body("suggestions[3].term", matchesPattern(spanRegExStarting))
+        .body("suggestions[0].term", matchesPattern(spanRegExStarting))
+        .body("suggestions[1].term", matchesPattern(spanRegExStarting))
+        .body("suggestions[2].term", matchesPattern(spanRegExStarting))
+        .body("suggestions[3].term", matchesPattern(spanRegExStarting))
         .body("products[0].name", matchesPattern(spanRegExIncludes))
-            .body("products[1].name", matchesPattern(spanRegExIncludes))
-            .body("products[2].name", matchesPattern(spanRegExIncludes))
-            .body("products[3].name", matchesPattern(spanRegExIncludes))
-            .body("pages[0].title", matchesPattern(nameWiley))
-            .body("pages[1].title", matchesPattern(nameWiley))
-            .body("pages[2].title", matchesPattern(nameWiley))
-            .body("pages[3].title", matchesPattern(nameWiley))
-            .extract().body().as(JsonNode.class);
+        .body("products[1].name", matchesPattern(spanRegExIncludes))
+        .body("products[2].name", matchesPattern(spanRegExIncludes))
+        .body("products[3].name", matchesPattern(spanRegExIncludes))
+        .body("pages[0].title", matchesPattern(nameWiley))
+        .body("pages[1].title", matchesPattern(nameWiley))
+        .body("pages[2].title", matchesPattern(nameWiley))
+        .body("pages[3].title", matchesPattern(nameWiley))
+        .extract().body().as(JsonNode.class);
     JsonNode images = node.findValues("images").get(0);
-    byte[] arr = given().
-    when().get(images.get(0).get("url").asText()).
-            asByteArray();
+    byte[] arr = given().when().get(images.get(0).get("url").asText()).asByteArray();
     BufferedImage image = ImageIO.read(new ByteArrayInputStream(arr));
-    Assert.assertEquals(image.getWidth(),300);
+    Assert.assertEquals(image.getWidth(), 300);
+  }
+
+  @Test
+  public void maxDelayed() {
+    await()
+        .atMost(10, TimeUnit.SECONDS)
+        .until(() -> this.postDelay() == 200);
   }
 }
